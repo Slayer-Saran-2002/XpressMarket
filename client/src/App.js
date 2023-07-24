05fc2314304ad81aa3595da9317b418f9d0bb9c2
Saranjeet Mondal <76931124+Slayer-Saran-2002@users.noreply.github.com>
2023-07-25 02:02:08 +0530

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,23 @@
-function App() {
-    return <div>App</div>;
-}
+import {BrowserRouter, Route,Routes}from "react-router-dom";
+import Home from './components/Home/Home';
+import Cart from './components/Cart/Cart';
+import SingleProduct from './components/SingleProduct/SingleProduct';
+import Category from './components/Category/Category';
+import AppContext from './utils/context';
 
-export default App;
+function App() {
+
+  return (
+    <BrowserRouter>
+      <AppContext>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/product:id' element={<SingleProduct/>}/>
+          <Route path='/category:id' element={<Category/>}/>
+        </Routes>
+      </AppContext>
+    </BrowserRouter>
+  ) 
+};
+
+export default App
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,24 @@
-import "./Header.scss";
-const Header = () => {
-    return <div>Header</div>;
-};
+import "Header.scss";
+import { useState,useContext,useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { CgShoppingCart } from "react-icons/cg";
+import { TbSearch } from "react-icons/tb";
+import { AiOutlineHeart } from "react-icons/ai";
+import Search from "./Search/Search"
+import Cart from './components/Cart';
+import {Context} from './utils/context'; 
 
+const Header =() =>{
+    return(
+        <header className="main-header">
+            <div className="header-content">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Categories</li>
+                </ul>
+            </div>
+        </header>
+        );
+};
 export default Header;

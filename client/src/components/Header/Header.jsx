ed879a04f437ddddb4556df2bfb1cd6850fa86af
Saranjeet Mondal <76931124+Slayer-Saran-2002@users.noreply.github.com>
2023-07-31 20:47:13 +0530

    Header done

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.scss";
-// import { useState,useContext,useEffect } from 'react';
+import { useState, useContext, useEffect } from 'react';
 // import { useNavigate } from "react-router-dom";
 import { CgShoppingCart } from "react-icons/cg";
 import { BiSearch } from "react-icons/bi";
@@ -7,9 +7,23 @@ import { AiOutlineHeart } from "react-icons/ai";
 // import Search from "./Search/Search"
 // import Cart from '../Cart/Cart';
 // import {Context} from '../../utils/context';
-const Header =() =>{
-    return(
-        <header className="main-header">
+const Header = () => {
+    const [scrolled, setScrolled] = useState(false);
+    const handlescroll = () => {
+        const offset = window.scrollY;
+        console.log(offset);
+        if (offset > 100) {
+            setScrolled(true);
+        }
+        else {
+            setScrolled(false);
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", handlescroll)
+    }, [])
+    return (
+        <header className={`main-header ${scrolled ? 'sticky-header' : ''} `}>
             <div className="header-content">
                 <ul className="left">
                     <li>Home</li>
@@ -18,14 +32,14 @@ const Header =() =>{
                 </ul>
                 <div className="center">XpressMart</div>
                 <div className="right">
-                    <BiSearch/>
-                    <AiOutlineHeart/>
-                    <span className="cart-icon"><CgShoppingCart/>
-                    <span>5</span>
+                    <BiSearch />
+                    <AiOutlineHeart />
+                    <span className="cart-icon"><CgShoppingCart />
+                        <span>5</span>
                     </span>
                 </div>
             </div>
         </header>
-        );
+    );
 };
 export default Header;
